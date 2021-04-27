943c1dbc4af85b83fd8c5184f0b61efedf7f1ae9
EsDeSepa <josepablocarrascocobos@hotmail.com>
2021-04-27 02:01:54 +0200

    Svelte Intento Nº 3

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,15 @@ import Router from 'svelte-spa-router';
 	import NotFound from "./NotFound.svelte";
 	import Home from "./Home.svelte";
 	import anxiety_stats from "./anxiety_stats_API/App.svelte";
+	import depression_stats from "./depression_stats_API/App.svelte";
+	import stress_stats from "./stress_stats_API/App.svelte";
+
 
 	const routes = {
 		"/": Home,
 		"/anxiety_stats": anxiety_stats,
+		"/depression_stats": depression_stats,
+		"/stress_stats": stress_stats,
 		"*": NotFound,
 	};
 </script>
