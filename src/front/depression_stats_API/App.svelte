100df98b25cd49e883214bc983e615778892b736
jdblanco <diezblancojuan@gmail.com>
2021-05-10 11:49:46 +0200

    prueba

--- src/front/depression_stats_API/App.svelte
+++ src/front/depression_stats_API/App.svelte
@@ -4,6 +4,6 @@
 
 <main>
     <style>body{background:lightorange}</style>
-    <h1 class="display-4" style="text-align:center;">API Depression_Stats - SOS2021</h1>
+    <h1 class="display-4" style="text-align:center;">Datos depresion</h1>
     <DepressionStatsTable/>
 </main>
