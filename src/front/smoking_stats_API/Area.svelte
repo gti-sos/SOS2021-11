459c248ba163fbb74c95d3efb216018c730c77da
Mirishya <campanocrespomiriam@gmail.com>
2021-06-03 13:42:30 +0200

    Gráfica intento 7

--- src/front/smoking_stats_API/Area.svelte
+++ src/front/smoking_stats_API/Area.svelte
@@ -87,16 +87,8 @@
             marker: {
                 lineWidth: 1,
                 lineColor: '#ffffff'
-            },
-            accessibility: {
-                pointDescriptionFormatter: function (point) {
-                    function round(x) {
-                        return Math.round(x * 100) / 100;
-                    }
-                    return (point.index + 1) + ', ' + point.category + ', ' +
-                        point.y + ' millions, ' + round(point.percentage) + '%, ' +
-                        point.series.name;
-                }
+            
+            
             }
         }
     },
