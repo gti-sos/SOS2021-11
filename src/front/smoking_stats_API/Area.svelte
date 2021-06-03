b52e383cb7be05239f24ee310eac5435ea8a0552
Mirishya <campanocrespomiriam@gmail.com>
2021-06-03 12:32:22 +0200

    Gráfica intento 1

--- src/front/smoking_stats_API/Area.svelte
+++ src/front/smoking_stats_API/Area.svelte
@@ -1,5 +1,190 @@
 <script>
+    import { Nav, NavItem, NavLink } from "sveltestrap";
+    var errorMsg = "";
+    var datos = [];
+    const BASE_API_AREA = "https://restcountries.eu/rest/v2/?fields=name;subregion;area"
+    
+    async function loadSport() {
+        console.log("Loading data...");
+      
+        const res = await fetch(BASE_API_AREA).then(
+          function (res) {
+            if (res.ok) {
+              errorMsg = "";
+              console.log("OK");
+            } else {
+              if (res.status === 500) {
+                errorMsg = "No se ha podido acceder a la base de datos";
+              }
+              console.log("ERROR!" + errorMsg);
+            }
+          }
+        );
+      }
+      
+      async function getDatos() {
+        console.log("Fetching data...");
+        await loadSport();
+        const res = await fetch(BASE_API_AREA);
+        if (res.ok) {
+          const json = await res.json();
+          datos = json;
+      
+          console.log(`We have received ${datos.length} stats.`);
+          console.log("Ok");
+        } else {
+          errorMsg = "Error recuperando datos";
+          console.log("ERROR!" + errorMsg);
+        }
+      }
+      async function loadChart(){
+        await getDatos();
+        var nombre = [];
+        var area = [] ;
+
+       
+        datos.forEach((dato_api) => {
+            nombre.push(dato_api.name);
+            area.push(dato_api.area);
+           
+    
+        });
+    }
+          
+              Highcharts.setOptions({
+    colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    })
+});
 
 
-    let prueba ="";
+Highcharts.chart('container', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Extensión de área mundial'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                connectorColor: 'silver'
+            }
+        }
+    },
+    series: [{
+        name: 'Share',
+        data: [
+            { nombre, area },
+            
+            
+        ]
+    }]
+});    
+            
+    
+      
     </script>
+    
+    <svelte:head>
+        
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+	<script src="https://code.highcharts.com/modules/exporting.js"></script>
+	<script src="https://code.highcharts.com/modules/export-data.js"></script>
+	<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
+	
+    </svelte:head>
+    
+     
+    <main> 
+      <Nav>
+          <NavItem>
+          <NavLink href="/">Página Principal</NavLink>
+          </NavItem>
+          <NavItem>
+          <NavLink href="#/integrations">Integraciones</NavLink>
+          </NavItem>
+          </Nav>          
+          
+      <h3>Uso de la API externa Extensión de Área Mundial</h3>
+      
+    
+      <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">    </p>
+      </figure>
+    
+    
+      {#if errorMsg}
+      <p>{errorMsg}</p>
+      {/if}
+    </main>
+    
+      
+    <style>
+      main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+      }
+     .highcharts-figure, .highcharts-data-table table {
+    min-width: 320px; 
+    max-width: 660px;
+    margin: 1em auto;
+}
+
+.highcharts-data-table table {
+	font-family: Verdana, sans-serif;
+	border-collapse: collapse;
+	border: 1px solid #EBEBEB;
+	margin: 10px auto;
+	text-align: center;
+	width: 100%;
+	max-width: 500px;
+}
+.highcharts-data-table caption {
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
+}
+.highcharts-data-table th {
+	font-weight: 600;
+    padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+    padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+    background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+    background: #f1f7ff;
+}
+
+    
+    </style>
