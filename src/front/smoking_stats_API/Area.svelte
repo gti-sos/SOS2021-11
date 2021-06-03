217a4a4e03c706ddd259d4d827b55139887f9433
Mirishya <campanocrespomiriam@gmail.com>
2021-06-03 14:21:13 +0200

    Gráfica intento ...

--- src/front/smoking_stats_API/Area.svelte
+++ src/front/smoking_stats_API/Area.svelte
@@ -51,130 +51,131 @@
         });
 		
 		
-		Highcharts.chart('container', {
-    chart: {
-        type: 'area',
-        inverted: true
-    },
-    title: {
-        text: 'Extensión de área mundial'
-    },
-    accessibility: {
-        keyboardNavigation: {
-            seriesNavigation: {
-                mode: 'serialize'
+		Highcharts.setOptions({
+            colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            })
+        });
+        
+        Highcharts.chart('container', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',
+                backgroundColor: null,
+                height: 500
+            },
+            title: {
+                text: 'Integración API Externa.'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        connectorColor: 'silver'
+                    }
+                }
+            },
+            series: [{
+                name: 'Comparación',
+                data: area
+            }],
+            responsive: {
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
             }
-        }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -150,
-        y: 100,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF'
-    },
-    xAxis: {
-        categories: nombre,
-    },
-    yAxis: {
-        title: {
-            text: 'N'
-        },
-        allowDecimals: true,
-        min: 0
-    },
-    plotOptions: {
-        area: {
-            fillOpacity: 0.5
-        }
-    },
-    series: [{
-        name: 'Área mundial',
-        data: area
-    
-    }]
-});
-}
-
+        });
+    }
 </script>
-    
-    <svelte:head>
-	<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
- </svelte:head>
- 
-  <main> 
-      <Nav>
+
+<svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadChart}"></script>
+</svelte:head>
+
+<main>
+
+    <Nav>
           <NavItem>
           <NavLink href="/">Página Principal</NavLink>
           </NavItem>
           <NavItem>
           <NavLink href="#/integrations">Integraciones</NavLink>
           </NavItem>
-          </Nav>          
-          
-      <h3>Uso de la API externa Area</h3>
-      
-    
-      <figure class="highcharts-figure">
+          </Nav>
+
+    <figure class="highcharts-figure">
         <div id="container"></div>
-        <p class="highcharts-description">    </p>
-      </figure>
-	  
-	   {#if errorMsg}
-      <p>{errorMsg}</p>
-      {/if}
-    </main>
-    
-      
-    <style>
-      main {
+        <p class="highcharts-description">
+            Integracion con una API Externa.
+        </p>
+    </figure> 	
+
+</main>
+
+<style>
+    .highcharts-figure, .highcharts-data-table table {
+        min-width: 320px; 
+        max-width: 660px;
+        margin: 1em auto;
+    }
+    .highcharts-data-table table {
+        font-family: Verdana, sans-serif;
+        border-collapse: collapse;
+        border: 1px solid #EBEBEB;
+        margin: 10px auto;
         text-align: center;
-        padding: 1em;
-        margin: 0 auto;
-      }
-	  
-	  #container {
-    height: 410px; 
-}
-
-.highcharts-figure, .highcharts-data-table table {
-    min-width: 320px; 
-    max-width: 800px;
-    margin: 1em auto;
-}
-
-.highcharts-data-table table {
-    font-family: Verdana, sans-serif;
-    border-collapse: collapse;
-    border: 1px solid #EBEBEB;
-    margin: 10px auto;
-    text-align: center;
-    width: 100%;
-    max-width: 500px;
-}
-.highcharts-data-table caption {
-    padding: 1em 0;
-    font-size: 1.2em;
-    color: #555;
-}
-.highcharts-data-table th {
-	font-weight: 600;
-    padding: 0.5em;
-}
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-    padding: 0.5em;
-}
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-    background: #f8f8f8;
-}
-.highcharts-data-table tr:hover {
-    background: #f1f7ff;
-}
+        width: 100%;
+        max-width: 500px;
+    }
+    .highcharts-data-table caption {
+        padding: 1em 0;
+        font-size: 1.2em;
+        color: #555;
+    }
+    .highcharts-data-table th {
+        font-weight: 600;
+        padding: 0.5em;
+    }
+    .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+        padding: 0.5em;
+    }
+    .highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+        background: #f8f8f8;
+    }
+    .highcharts-data-table tr:hover {
+        background: #f1f7ff;
+    }
 </style>
