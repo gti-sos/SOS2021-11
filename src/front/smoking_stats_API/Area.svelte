62d45189ac8719a7fd1e49faf4fefb6d4b12fbee
Mirishya <campanocrespomiriam@gmail.com>
2021-06-03 13:58:23 +0200

    Gráfica intento 9

--- src/front/smoking_stats_API/Area.svelte
+++ src/front/smoking_stats_API/Area.svelte
@@ -106,8 +106,7 @@
 	<script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}>
-</script>
+<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
  </svelte:head>
  
   <main> 
