1d614270e40ab0980348e3cbefc54df1b6a7a4ae
anaromero99 <anaromerocaceres@gmail.com>
2021-06-04 11:30:19 +0200

    apis intento 1

--- src/front/stress_stats_API/Desempleo.svelte
+++ src/front/stress_stats_API/Desempleo.svelte
@@ -1,6 +1,178 @@
 <script>
-
-
-    let prueba="";
+    import { Nav, NavItem, NavLink } from "sveltestrap";
+    var errorMsg = "";
+    var datos = [];
+    const BASE_API_DESEMPLEO = "https://sos2021-07.herokuapp.com/api/v2/unemployment"
+    //INTEGRACION API INTERNA
+    async function loadDesempleo() {
+        console.log("Loading data...");
+        const res = await fetch(BASE_API_DESEMPLEO).then(
+          function (res) {
+            if (res.ok) {
+              errorMsg = "";
+              console.log("OK");
+            } else {
+              if (res.status === 500) {
+                errorMsg = "No se ha podido acceder a la base de datos";
+              }
+              console.log("ERROR!" + errorMsg);
+            }
+          }
+        );
+      }
     
-    </script>
+      async function getDatos() {
+        console.log("Fetching data...");
+        await loadDesempleo();
+        const res = await fetch(BASE_API_DESEMPLEO);
+        if (res.ok) {
+          const json = await res.json();
+          datos = json;
+          console.log(`We have received ${datos.length} stats.`);
+          console.log("Ok");
+        } else {
+          errorMsg = "Error recuperando datos";
+          console.log("ERROR!" + errorMsg);
+        }
+      }
+      async function loadChart(){
+        await getDatos();
+        var tasa = [];
+        var ocupacion = [] ;
+        var año = [] ;
+        datos.forEach((dato_desempleo) => {
+            tasa.push(dato_desempleo.unemployment_rate);
+            ocupacion.push(dato_desempleo.occupation_variation);
+            año.push(dato_desempleo.year);
+        });
+        
+        Highcharts.chart('container', {
+
+    chart: {
+        type: 'columnrange',
+        inverted: true
+    },
+
+    title: {
+        text: 'Tasa de desempleo por año'
+    },
+
+    xAxis: {
+        categories: año,
+    },
+
+    yAxis: {
+        title: {
+            text: 'Tasa'
+        }
+    },
+
+    tooltip: {
+        valueSuffix: '°C'
+    },
+
+    plotOptions: {
+        columnrange: {
+            dataLabels: {
+                enabled: true,
+                format: '{y}°C'
+            }
+        }
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Tasa',
+        data: tasa   
+    }, {
+        name: 'Ocupación',
+        data: ocupacion
+    }]
+
+});
+}
+</script>
+
+<svelte:head>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
+</svelte:head>
+
+<main> 
+    <Nav>
+        <NavItem>
+        <NavLink href="/">Página Principal</NavLink>
+        </NavItem>
+        <NavItem>
+        <NavLink href="#/integrations">Integraciones</NavLink>
+        </NavItem>
+        </Nav> 
+
+<figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+    </p>
+</figure>
+
+
+{#if errorMsg}
+<p>{errorMsg}</p>
+{/if}
+</main>
+
+
+<style>
+    main {
+      text-align: center;
+      padding: 1em;
+      margin: 0 auto;
+    }
+    
+
+.highcharts-figure, .highcharts-data-table table {
+  min-width: 320px; 
+  max-width: 800px;
+  margin: 1em auto;
+}
+
+#container {
+  height: 400px;
+}
+
+.highcharts-data-table table {
+font-family: Verdana, sans-serif;
+border-collapse: collapse;
+border: 1px solid #EBEBEB;
+margin: 10px auto;
+text-align: center;
+width: 100%;
+max-width: 500px;
+}
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+.highcharts-data-table th {
+font-weight: 600;
+  padding: 0.5em;
+}
+.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+  padding: 0.5em;
+}
+.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
+
+
+
+</style>
