8111d0cfbb01d2280d79ace21957c2923f474925
Mirishya <campanocrespomiriam@gmail.com>
2021-06-04 13:57:34 +0200

    Gráfica interna intento 5

--- src/front/smoking_stats_API/Grammys.svelte
+++ src/front/smoking_stats_API/Grammys.svelte
@@ -52,145 +52,30 @@
            
     
         });
-        
-         var colors = Highcharts.getOptions().colors;
-Highcharts.chart('container', {
+        var chart = new tauCharts.Chart({
 
-    chart: {
-        type: 'streamgraph',
-        marginBottom: 30,
-        zoomType: 'x'
-    },
+data:premios,
+type:'scatterplot',
+x: anyo,
+y: nombre,
+color:'isWinner',
+size:null,
+plugins:
+[
 
-    // Make sure connected countries have similar colors
-    colors: [
-        colors[0],
-        colors[1],
-        colors[2],
-        colors[3],
-        colors[4],
-        // East Germany, West Germany and Germany
-        Highcharts.color(colors[5]).brighten(0.2).get(),
-        Highcharts.color(colors[5]).brighten(0.1).get(),
-
-        colors[5],
-        colors[6],
-        colors[7],
-        colors[8],
-        colors[9],
-        colors[0],
-        colors[1],
-        colors[3],
-        // Soviet Union, Russia
-        Highcharts.color(colors[2]).brighten(-0.1).get(),
-        Highcharts.color(colors[2]).brighten(-0.2).get(),
-        Highcharts.color(colors[2]).brighten(-0.3).get()
-    ],
-
-    title: {
-        floating: true,
-        align: 'left',
-        text: 'Premios de grammys por artista y año'
-    },
-    subtitle: {
-        floating: true,
-        align: 'left',
-        y: 30,
-        text: ''
-    },
-
-    xAxis: {
-        maxPadding: 0,
-        type: 'category',
-        crosshair: true,
-        categories: nombre,
-        labels: {
-            align: 'left',
-            reserveSpace: false,
-            rotation: 270
-        },
-        lineWidth: 0,
-        margin: 20,
-        tickWidth: 0
-    },
-
-    yAxis: {
-        visible: false,
-        startOnTick: false,
-        endOnTick: false
-    },
-
-    legend: {
-        enabled: false
-    },
-
-    annotations: [{
-        labels: [{
-            point: {
-                x: 5.5,
-                xAxis: 0,
-                y: 30,
-                yAxis: 0
-            },
-            text: 'Premios'
-        }, {
-            point: {
-                x: 18,
-                xAxis: 0,
-                y: 90,
-                yAxis: 0
-            },
-            text: ''
-        }],
-        labelOptions: {
-            backgroundColor: 'rgba(255,255,255,0.5)',
-            borderColor: 'silver'
-        }
-    }],
-
-    plotOptions: {
-        series: {
-            label: {
-                minFontSize: 5,
-                maxFontSize: 15,
-                style: {
-                    color: 'rgba(255,255,255,0.75)'
-                }
-            }
-        }
-    },
-
-    
-    series: [{
-        name: "Premios",
-        data: premios
-    }, {
-        name: "Año",
-        data: anyo
-    }
-    
-     
-    ],
-
-    exporting: {
-        sourceWidth: 800,
-        sourceHeight: 600
-    }
+tauCharts.api.plugins.get('tooltip')(),
+tauCharts.api.plugins.get('legend')(),
+tauCharts.api.plugins.get('quick-filter')(),
+tauCharts.api.plugins.get('trendline')(),
+]
 
 });
-      }
+
+chart.renderTo('#container'); 
+}
     </script>
     
-    <svelte:head>
-        
-	<script src="https://code.highcharts.com/highcharts.js"></script>
-	<script src="https://code.highcharts.com/modules/streamgraph.js"></script>
-	<script src="https://code.highcharts.com/modules/series-label.js"></script>
-	<script src="https://code.highcharts.com/modules/annotations.js"></script>
-	<script src="https://code.highcharts.com/modules/exporting.js"></script>
-	<script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
-    </svelte:head>
+    
     
      
     <main> 
@@ -205,12 +90,6 @@ Highcharts.chart('container', {
           
       <h3>Uso de la API interna del Grupo 22 de SOS Grammys</h3>
       
-    
-      <figure class="highcharts-figure">
-        <div id="container"></div>
-        <p class="highcharts-description">    </p>
-      </figure>
-    
     
       {#if errorMsg}
       <p>{errorMsg}</p>
@@ -228,39 +107,6 @@ Highcharts.chart('container', {
 	height: 600px;
 }
 
-.highcharts-figure, .highcharts-data-table table {
-    min-width: 310px; 
-	max-width: 800px;
-	overflow: auto;
-    margin: 1em auto;
-}
 
-.highcharts-data-table table {
-	font-family: Verdana, sans-serif;
-	border-collapse: collapse;
-	border: 1px solid #EBEBEB;
-	margin: 10px auto;
-	text-align: center;
-	width: 100%;
-	max-width: 500px;
-}
-.highcharts-data-table caption {
-    padding: 1em 0;
-    font-size: 1.2em;
-    color: #555;
-}
-.highcharts-data-table th {
-	font-weight: 600;
-    padding: 0.5em;
-}
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-    padding: 0.5em;
-}
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-    background: #f8f8f8;
-}
-.highcharts-data-table tr:hover {
-    background: #f1f7ff;
-}
 
     </style>
