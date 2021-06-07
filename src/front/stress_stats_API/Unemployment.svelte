0c649daeec4d701b27802b4e11063bd54362f522
anaromero99 <anaromerocaceres@gmail.com>
2021-06-07 10:10:06 +0200

    GRAFICA UNEMPLOYMENT

--- src/front/stress_stats_API/Unemployment.svelte
+++ src/front/stress_stats_API/Unemployment.svelte
@@ -1,74 +1,82 @@
 <script>
-    import Button from "sveltestrap/src/Button.svelte";
-    import { pop } from "svelte-spa-router";
-    var miAPI = "https://sos2021-23.herokuapp.com/api/v2/unemployment-stats";
-    var API2 = "https://sos2021-11.herokuapp.com/api/v2/stress_stats";
-    async function loadGraph(){
-        let dataG2 = [];
-        let myData = [];
-        let datosPaisUnemp=[];
-        let datosAñosUnemp = [];
-        let datosIntperc = [];
-        let stress = [];
-        let datosAñosLife = [];
-        let datosPaisLife =[];
-               
-        const resDataG2 = await fetch(API2);
-        const resData = await fetch(miAPI);
+import { select_multiple_value } from "svelte/internal";
 
-        myData = await resData.json();
-        dataG2 = await resDataG2.json();
 
-        datosPaisUnemp = myData.map((myData)=> (myData.country));
-        datosAñosUnemp = myData.map((myData)=> parseInt(myData.year));
-        datosIntperc = myData.map((myData)=> myData.intperc);
-        
-        datosPaisLife = dataG2.map((dataG2)=> dataG2.country);
-        datosAñosLife = dataG2.map((dataG2)=> dataG2.date);
-        stress = dataG2.map((dataG2)=> dataG2.stress_population);
-        
-        dataG2.forEach((e) => {
-            stress.push(e.stress_population);
-        });
+  var miAPI = "https://sos2021-23.herokuapp.com/api/v2/unemployment-stats/?year=2017";
+  var API2 = "https://sos2021-11.herokuapp.com/api/v2/stress_stats/?year=2017";
+  async function loadGraph(){
+      let dataG2 = [];
+      let myData = [];
+             
+      const resDataG2 = await fetch(API2);
+      const resData = await fetch(miAPI);
+
+      myData = await resData.json();
+      dataG2 = await resDataG2.json();
+
+      let array1 = [];
+      let array2 =[];
+      let sum = [];
+
+      dataG2.forEach(( x) =>{
+     sum +=parseFloat(x.stress_population)
+
+     console.log(dataG2)
+     console.log((x.gfperc))
+     
+      });
+
+      array1.push({x:"2017".toString(), y:parseFloat(sum) })
+
+      
+
+  myData.forEach(( x) =>{
+     array2.push({x: x.year.toString(), y:parseFloat( x.gfperc)})
+     console.log(myData)
+    });
+
+    console.log(array1)
+    console.log(array2)
     
-        var chart = JSC.chart('chartDiv', { 
-debug: true, 
-type: 'horizontal column', 
-palette: 'fiveColor7', 
-yAxis: { 
-  scale_type: 'stacked', 
-  label_text: 'Units Sold'
-}, 
-defaultPoint_outline_color: 'darkenMore', 
-title_label_text: 'Acme Tool Sales', 
-xAxis_label_text: 'Quarter', 
-series: [ 
-  { 
-    name: 'Saw', 
-    id: 's1', 
-    points: datosIntperc 
+
+      console.log("OK, BIEN")
+      var chart = JSC.chart('chartDiv', { 
+  debug: true, 
+  type: 'horizontal column', 
+  palette: 'fiveColor7', 
+  yAxis: { 
+    scale_type: 'stacked', 
+    label_text: 'porcentaje'
   }, 
-  { 
-    name: 'Saw', 
-    id: 's1', 
-    points: stress 
-  }, 
-  
-] 
-});
-  
-  }
+  defaultPoint_outline_color: 'darkenMore', 
+  title_label_text: 'Acme Tool Sales', 
+  xAxis_label_text: 'Año', 
+  series: [ 
+    { 
+      name: 'Stress', 
+      id: 's1', 
+      points: 
+        array1
+      
+    }, 
+    { 
+      name: 'Unemployment', 
+      points: array2
+    } 
+  ] 
+}); 
+}
 </script>
 
 <svelte:head>
 
-  <script
-  type="text/javascript" src="https://code.jscharting.com/latest/jscharting.js"
-      on:load={loadGraph}></script>
+<script
+type="text/javascript" src="https://code.jscharting.com/latest/jscharting.js"
+    on:load={loadGraph}></script>
 </svelte:head>
 
 <main>
 
-  <div id="chartDiv"></div>
-  
+<div id="chartDiv"></div>
+
 </main>
