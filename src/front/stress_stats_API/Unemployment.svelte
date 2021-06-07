fc45627ba0199071113bd6e74ba51bdd035ea011
anaromero99 <anaromerocaceres@gmail.com>
2021-06-07 02:09:07 +0200

    QUE ASCO

--- src/front/stress_stats_API/Unemployment.svelte
+++ src/front/stress_stats_API/Unemployment.svelte
@@ -1,229 +1,74 @@
 <script>
-    import { Nav, NavItem, NavLink } from "sveltestrap";
-    var errorMsg = "";    
-    let correctMsg = "";
-    var datos = [];
-    var stress = [];
-    const BASE_API_URL_STRESS = "/api/v2/stress_stats";
-    const BASE_API_URL_DESEMPLEO = "/api/v2/unemployment-stats";
-    
-    //INTEGRACION GRUPO 23
-  async function loadDesempleo() {
-    console.log("Loading data...");
-    const res = await fetch(
-      BASE_API_URL_STRESS + "/loadInitialData"
-    ).then(function (res) {
-      if (res.ok) {
-        errorMsg = "";
-        correctMsg = "Datos cargados correctamente";
-        console.log("OK");
-      } else {
-        if (res.status === 500) {
-          errorMsg = "No se ha podido acceder a la base de datos";
-        }
-        correctMsg = "";
-        console.log("ERROR!" + errorMsg);
-      }
-    });
-  }
-  async function getDesempleo() {
-    console.log("Fetching data...");
-    await loadDesempleo();
-    const res = await fetch(BASE_API_URL_STRESS);
-    if (res.ok) {
-      console.log("OK");
-      stress = await res.json();
-      correctMsg = "";
-      console.log(`We have received ${stress.length} stress-stats.`);
-    } else {
-      console.log("Error");
-      errorMsg = "Error al cargar los datos de la API";
-    }
-  }
-    async function loadAPIDesempleo() {
-        console.log("Loading data...");
-        const res = await fetch(BASE_API_URL_DESEMPLEO).then(
-          function (res) {
-            if (res.ok) {
-              errorMsg = "";
-              console.log("OK");
-            } else {
-              if (res.status === 500) {
-                errorMsg = "No se ha podido acceder a la base de datos";
-              }
-              console.log("ERROR!" + errorMsg);
-            }
-          }
-        );
-      }
-    
-      
-      async function getDatos() {
-        console.log("Fetching data...");
-        await loadAPIDesempleo();
-        const res = await fetch(BASE_API_URL_DESEMPLEO);
-    
-        if (res.ok) {
-          const json = await res.json();
-          datos = json;
-          console.log(`We have received ${datos.length} stats.`);    
-          console.log("Ok");
-        } else {
-          errorMsg = "Error recuperando datos";
-          console.log("ERROR!" + errorMsg);
-        }
-      }
-    
-      async function loadChart(){
-        await getDesempleo();
-        await getDatos();
-    
-       
-        var pais =[];
-        var porcentaje = [];
-        var stress_pop=[];
-       
-        datos.filter(datos => datos.year == 2017 ).forEach(d => { 
-           
-            stress.forEach((data) => {               
-                if(data.date==2017){
-                    pais.push((d.country));
-                    porcentaje.push(parseFloat(d.gfperc));
-                    stress_pop.push(parseFloat(data.stress_population));
-                }
-            })
+    import Button from "sveltestrap/src/Button.svelte";
+    import { pop } from "svelte-spa-router";
+    var miAPI = "https://sos2021-23.herokuapp.com/api/v2/unemployment-stats";
+    var API2 = "https://sos2021-11.herokuapp.com/api/v2/stress_stats";
+    async function loadGraph(){
+        let dataG2 = [];
+        let myData = [];
+        let datosPaisUnemp=[];
+        let datosAñosUnemp = [];
+        let datosIntperc = [];
+        let stress = [];
+        let datosAñosLife = [];
+        let datosPaisLife =[];
+               
+        const resDataG2 = await fetch(API2);
+        const resData = await fetch(miAPI);
+
+        myData = await resData.json();
+        dataG2 = await resDataG2.json();
+
+        datosPaisUnemp = myData.map((myData)=> (myData.country));
+        datosAñosUnemp = myData.map((myData)=> parseInt(myData.year));
+        datosIntperc = myData.map((myData)=> myData.intperc);
+        
+        datosPaisLife = dataG2.map((dataG2)=> dataG2.country);
+        datosAñosLife = dataG2.map((dataG2)=> dataG2.date);
+        stress = dataG2.map((dataG2)=> dataG2.stress_population);
+        
+        dataG2.forEach((e) => {
+            stress.push(e.stress_population);
         });
-
-
-Highcharts.chart('container', {
-    title: {
-        text: 'Highcharts Histogram'
-    },
-
-    xAxis: [{
-        title: { text: 'Data' },
-        alignTicks: false
-    }, {
-        title: { text: 'Histogram' },
-        alignTicks: false,
-        opposite: true
-    }],
-
-    yAxis: [{
-        title: { text: 'Data' }
-    }, {
-        title: { text: 'Histogram' },
-        opposite: true
-    }],
-
-    plotOptions: {
-        histogram: {
-            accessibility: {
-                pointDescriptionFormatter: function (point) {
-                    var ix = point.index + 1,
-                        x1 = point.x.toFixed(3),
-                        x2 = point.x2.toFixed(3),
-                        val = point.y;
-                    return ix + '. ' + x1 + ' to ' + x2 + ', ' + val + '.';
-                }
-            }
-        }
-    },
-
-    series: [{
-        name: 'Histogram',
-        type: 'histogram',
-        xAxis: 1,
-        yAxis: 1,
-        data: stress_pop,
-        baseSeries: 's1',
-        zIndex: -1
-    }, {
-        name: 'Data',
-        type: 'histogram',
-        data: pais,
-        id: 's1',
-        marker: {
-            radius: 1.5
-        }
-    }]
+    
+        var chart = JSC.chart('chartDiv', { 
+debug: true, 
+type: 'horizontal column', 
+palette: 'fiveColor7', 
+yAxis: { 
+  scale_type: 'stacked', 
+  label_text: 'Units Sold'
+}, 
+defaultPoint_outline_color: 'darkenMore', 
+title_label_text: 'Acme Tool Sales', 
+xAxis_label_text: 'Quarter', 
+series: [ 
+  { 
+    name: 'Saw', 
+    id: 's1', 
+    points: datosIntperc 
+  }, 
+  { 
+    name: 'Saw', 
+    id: 's1', 
+    points: stress 
+  }, 
+  
+] 
 });
-
-    }
-
-    </script>
+  
+  }
+</script>
 
 <svelte:head>
-        
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/histogram-bellcurve.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadChart}></script>
+
+  <script
+  type="text/javascript" src="https://code.jscharting.com/latest/jscharting.js"
+      on:load={loadGraph}></script>
 </svelte:head>
 
-<main> 
-    <Nav>
-        <NavItem>
-        <NavLink href="/">Página Principal</NavLink>
-        </NavItem>
-        <NavItem>
-        <NavLink href="#/integrations">Integraciones</NavLink>
-        </NavItem>
-        </Nav>          
-        
-    <h3>Uso de la API Interna Desempleo</h3>
-    
-  
-    <figure class="highcharts-figure">
-      <div id="container"></div>
-      <p class="highcharts-description">    </p>
-    </figure>
-  
-  
-    {#if errorMsg}
-    <p>{errorMsg}</p>
-    {/if}
-  </main>
+<main>
 
-      
-  <style>
-    main {
-      text-align: center;
-      padding: 1em;
-      margin: 0 auto;
-    }
-    .highcharts-figure, .highcharts-data-table table {
-  min-width: 310px; 
-  max-width: 800px;
-  margin: 1em auto;
-}
-
-.highcharts-data-table table {
-font-family: Verdana, sans-serif;
-border-collapse: collapse;
-border: 1px solid #EBEBEB;
-margin: 10px auto;
-text-align: center;
-width: 100%;
-max-width: 500px;
-}
-.highcharts-data-table caption {
-  padding: 1em 0;
-  font-size: 1.2em;
-  color: #555;
-}
-.highcharts-data-table th {
-font-weight: 600;
-  padding: 0.5em;
-}
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-  padding: 0.5em;
-}
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-  background: #f8f8f8;
-}
-.highcharts-data-table tr:hover {
-  background: #f1f7ff;
-}
-</style>
-
+  <div id="chartDiv"></div>
+  
+</main>
